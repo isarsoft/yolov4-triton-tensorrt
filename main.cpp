ed80a326de20e597faff98ad43bd0ba66321223f
Philipp Schmidt <p.schmidt@isarsoft.com>
2020-08-16 12:13:24 +0200

    Add progress output

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "utils/logging.h"
 static Logger gLogger;
 
+#include <iostream>
+
 #define DEVICE 0
 #define BATCH_SIZE 1
 
@@ -14,14 +16,17 @@ using namespace nvinfer1;
 int main(int argc, char** argv) {
     cudaSetDevice(DEVICE);
 
+    std::cout << "Creating builder" << std::endl;
     // Create builder
     IBuilder* builder = createInferBuilder(gLogger);
     IBuilderConfig* config = builder->createBuilderConfig();
 
+    std::cout << "Creating model" << std::endl;
     // Create model to populate the network, then set the outputs and create an engine
     ICudaEngine* engine = yolov4::createEngine(BATCH_SIZE, builder, config, DataType::kFLOAT, "../yolov4.wts");
     assert(engine != nullptr);
 
+    std::cout << "Serializing model to engine file" << std::endl;
     // Serialize the engine
     IHostMemory* modelStream{nullptr};
     modelStream = engine->serialize();
@@ -33,11 +38,13 @@ int main(int argc, char** argv) {
     assert(modelStream != nullptr);
     std::ofstream p("yolov4.engine", std::ios::binary);
     if (!p) {
-        std::cerr << "could not open plan output file" << std::endl;
+        std::cerr << "Could not open engine output file yolov4.engine" << std::endl;
         return -1;
     }
     p.write(reinterpret_cast<const char*>(modelStream->data()), modelStream->size());
     modelStream->destroy();
+
+    std::cout << "Done" << std::endl;
 
     return 0;
 }
